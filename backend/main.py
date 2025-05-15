56816d56567ecaadf277a99d1a370f7b832e46fd
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-16 01:10:25 +0530

    Refactored - Add routes in seperate file for user

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,10 @@ from typing import Annotated, List, Optional
 from fastapi import FastAPI, Query, status, Response, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from models import User
-import models, schemas, utils
-from database import get_db, engine, Base
-
+from .models import User
+from . import models, schemas, utils
+from .database import get_db, engine, Base
+from .routers import user
 
 
 Base.metadata.create_all(bind=engine)
@@ -17,22 +17,25 @@ def root():
     return "This is currently running at port 8000"
 
 
-@app.post("/users/", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    hashed_password = utils.hash_password(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+# @app.post("/users/", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     hashed_password = utils.hash_password(user.password)
+#     user.password = hashed_password
+#     new_user = models.User(**user.model_dump())
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
 
-    return new_user
+#     return new_user
 
-@app.get("/users/{id}", response_model=schemas.UserResponse, status_code=status.HTTP_202_ACCEPTED)
-def get_user(id : int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
+app.include_router(user.router)
 
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id:{id} not found')
 
-    return user
+# @app.get("/users/{id}", response_model=schemas.UserResponse, status_code=status.HTTP_202_ACCEPTED)
+# def get_user(id : int, db: Session = Depends(get_db)):
+#     user = db.query(models.User).filter(models.User.id == id).first()
+
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id:{id} not found')
+
+#     return user
---
+++ backend/routers/user.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, status, Response, HTTPException, Depends
+from sqlalchemy.orm import Session
+from .. import models, schemas, utils
+from backend.database import get_db
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+@router.post("", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash_password(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+@router.get("/{id}", response_model=schemas.UserResponse, status_code=status.HTTP_202_ACCEPTED)
+def get_user(id : int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id:{id} not found')
+
+    return user
