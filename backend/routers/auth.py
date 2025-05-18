e092d10575c3846bc0728d1b2cd82e90bc3025f7
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-18 23:39:04 +0530

    Added return_access_token when successful login

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from .. import schemas, models, utils
 from ..database import get_db
+from . import oauth2
 
 
 router = APIRouter(tags=["Authentication"])
@@ -20,5 +21,6 @@ def login(user_cred: schemas.UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials"
         )
+    access_token = oauth2.create_access_token(data={"user_id": str(user.id)})
 
-    return "success"
+    return {"access_token": access_token, "token_type": "bearer"}
