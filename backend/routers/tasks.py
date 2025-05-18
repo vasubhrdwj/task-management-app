d394b3c992f1f16ef019859d159f0669a3f60649
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-18 23:30:25 +0530

    added authorization to create_task route

--- backend/routers/tasks.py
+++ backend/routers/tasks.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status, Response, HTTPException, Depends
 from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from backend.database import get_db
+from ..routers import oauth2
 
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
@@ -11,8 +12,12 @@ router = APIRouter(prefix="/tasks", tags=["Tasks"])
 @router.post(
     "/create", response_model=schemas.TaskResponse, status_code=status.HTTP_201_CREATED
 )
-def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    new_task = models.Tasks(**task.model_dump())
+def create_task(
+    task: schemas.TaskCreate,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(oauth2.get_current_user),
+):
+    new_task = models.Tasks(owner_id=current_user.id, **task.model_dump())
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
