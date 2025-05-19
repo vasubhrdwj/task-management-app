561db8da50a642aa4449ca8ae6fac468ecffe32f
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-19 21:25:41 +0530

    Added condition to raise error if email exists in User creation (conflicting emails)

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -5,11 +5,27 @@ from backend.database import get_db
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/create", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
+
+@router.post(
+    "/create", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED
+)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    check_existing_user = (
+        db.query(models.User).filter(models.User.email == user.email).first()
+    )
+
+    if check_existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="User Already Exists"
+        )
+
     hashed_password = utils.hash_password(user.password)
     user.password = hashed_password
+
     new_user = models.User(**user.model_dump())
+    print(new_user)
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
@@ -17,11 +33,15 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get("/{id}", response_model=schemas.UserResponse, status_code=status.HTTP_202_ACCEPTED)
-def get_user(id : int, db: Session = Depends(get_db)):
+@router.get(
+    "/{id}", response_model=schemas.UserResponse, status_code=status.HTTP_202_ACCEPTED
+)
+def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'user with id:{id} not found')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id:{id} not found"
+        )
 
     return user
