a345cee29fd49785c1d241c64a2d2253e4a43953
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-19 19:21:46 +0530

    Added authentication to get_task endpoint

--- backend/routers/tasks.py
+++ backend/routers/tasks.py
@@ -51,12 +51,7 @@ def get_task(
     response_model=schemas.TaskResponse,
     status_code=status.HTTP_202_ACCEPTED,
 )
-def update_task(
-    task: schemas.TaskCreate,
-    id: int,
-    db: Session = Depends(get_db),
-    current_user: models.User = Depends(oauth2.get_current_user),
-):
+def update_task(task: schemas.TaskCreate, id: int, db: Session = Depends(get_db)):
     query_task = db.query(models.Tasks).filter(models.Tasks.id == id)
     query_task_instance = query_task.first()
 
