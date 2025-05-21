606ce0553463cdcd5de86353f3459dd0b463c5d3
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-21 16:48:08 +0530

    Added alembic for db migrations

---
+++ backend/alembic/versions/0f16d66ff33a_first_migrations.py
@@ -0,0 +1,28 @@
+"""first migrations
+
+Revision ID: 0f16d66ff33a
+Revises: 
+Create Date: 2025-05-21 16:28:56.912675
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0f16d66ff33a'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
