0abca4275bbfb3a7c4f727edfd8b342be24788d3
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-30 18:45:46 +0530

    Changed hardcoded request_body to input values

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import api from "./api";
 
 const App = () => {
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleLogin = async () => {
     try {
+      console.log(email);
       const response = await api.post(
         "/login",
         new URLSearchParams({
-          username: "vasu@gmail.com",
-          password: "vasu123",
+          username: email,
+          password: password,
         }),
         {
           headers: {
@@ -29,13 +30,23 @@ const App = () => {
         <label htmlFor="email" className="form-label">
           Email Address:
         </label>
-        <input type="email" className="form-control" id="email" />
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="password" className="form-label">
           Password:
         </label>
-        <input type="password" className="form-control" id="password" />
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div className="login-btn">
         <button type="submit" onClick={handleLogin}>
