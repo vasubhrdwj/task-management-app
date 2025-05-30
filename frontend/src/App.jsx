6f138c3077a104054a1dbc92cceb9678fe2030e6
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-30 18:39:16 +0530

    Added and checked axios request to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "./api";
 
 const App = () => {
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    try {
+      const response = await api.post(
+        "/login",
+        new URLSearchParams({
+          username: "vasu@gmail.com",
+          password: "vasu123",
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <div>
@@ -14,6 +36,11 @@ const App = () => {
           Password:
         </label>
         <input type="password" className="form-control" id="password" />
+      </div>
+      <div className="login-btn">
+        <button type="submit" onClick={handleLogin}>
+          Login
+        </button>
       </div>
     </div>
   );
