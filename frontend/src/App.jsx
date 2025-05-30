46c3b76fe7bea7ce66d5877f14538222abc3b74a
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-05-30 22:56:12 +0530

    Added routes for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Login from "./pages/LoginPage";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={Login}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </div>
   );
