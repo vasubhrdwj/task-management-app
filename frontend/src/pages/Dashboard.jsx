9f15fc38ca2e4afb671d6ca3e71302b8c4626d97
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-06-12 00:56:57 +0530

    Cleaned up Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -22,11 +22,11 @@ const Dashboard = () => {
     setSortParams({ sort_by, sort_desc });
   };
 
-  const {
-    data: userList,
-    isLoading: usersLoading,
-    error: usersError,
-  } = useUsers(user && initialized);
+  // const {
+  //   data: userList,
+  //   isLoading: usersLoading,
+  //   error: usersError,
+  // } = useUsers(user && initialized);
 
   if (!initialized) {
     return null;
@@ -37,34 +37,14 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="h-screen bg-amber-950 w-screen">
-      <div className="bg-amber-50 h-1/10 flex justify-center gap-15 center p-4 ">
+    <div className="h-screen bg-neutral-100 w-screen">
+      <div className="absolute right-10">
         <div className="font-medium">{user.email}</div>
         <div>{user.full_name}</div>
-        <div>{user.is_admin ? "Admin Privilege" : ""}</div>
       </div>
       <div className="flex h-9/10">
-        {/* Users Display */}
-        <div className="basis-1/5 bg-amber-50">
-          <h3 className="font-bold text-xl m-5 mb-8">Users:</h3>
-          {usersLoading && <p>Loading users...</p>}
-          {usersError && <p className="text-red-600">{usersError}</p>}
-          <ul>
-            {userList?.map((u) => (
-              <li
-                key={u.email}
-                className="border-1 rounded-md border-fuchsia-200 p-5 m-4 bg-fuchsia-200 text-center"
-              >
-                {u.full_name}
-                <br />
-                {u.is_admin ? "Is Admin" : "Not Admin"}
-              </li>
-            ))}
-          </ul>
-        </div>
-
         {/* Tasks Display */}
-        <div className="basis-4/5 bg-gray-50 p-10">
+        <div className="basis-4/5  p-10">
           <div className="pl-4 pr-6 flex justify-between mb-10">
             <h1 className="font-bold text-2xl">Tasks:</h1>
             <div className="place-self-end">
@@ -102,7 +82,5 @@ const Dashboard = () => {
     </div>
   );
 };
-
-
 
 export default Dashboard;
