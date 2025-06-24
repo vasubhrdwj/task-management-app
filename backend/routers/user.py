08a0f32f6bf2eb2d133467cb3c41f04652ce140a
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-06-24 23:53:43 +0530

    Added a get_user_by_id endpoint

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from backend.database import get_db
 from . import oauth2
+from uuid import UUID
 from typing import List
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -37,6 +38,23 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @router.get("/me", response_model=schemas.UserResponse, status_code=status.HTTP_200_OK)
 def read_users_me(current_user: models.User = Depends(oauth2.get_current_user)):
     return current_user
+
+
+@router.get(
+    "/byid/{id}",
+    response_model=schemas.UserResponse,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def get_user_by_id(id: UUID, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with email:{id} not found",
+        )
+
+    return user
 
 
 @router.get(
